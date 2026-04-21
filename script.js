7f216ee16d0aff98c64e312c233222b29bf6f5dd
Ririnosaa <nosariemenrejoice@gmail.com>
2026-04-21 11:47:52 +0100

    js code amended adjustment

--- script.js
+++ script.js
@@ -2,55 +2,74 @@ const firstAnswer = document.getElementById("first-answer");
 const secondAnswer = document.getElementById("second-answer");
 const thirdAnswer = document.getElementById("third-answer");
 const fourthAnswer = document.getElementById("fourth-answer");
+
 const firstButton = document.getElementById("first-button");
 const secondButton = document.getElementById("second-button");
 const thirdButton = document.getElementById("third-button");
 const fourthButton = document.getElementById("fourth-button");
 
+
+
+function closeAllAnswers() {
+    firstAnswer.classList.add("hidden");
+    secondAnswer.classList.add("hidden");
+    thirdAnswer.classList.add("hidden");
+    fourthAnswer.classList.add("hidden");
+
+    firstButton.querySelector("img").src = "image/icon-plus.svg";
+    secondButton.querySelector("img").src = "image/icon-plus.svg";
+    thirdButton.querySelector("img").src = "image/icon-plus.svg";
+    fourthButton.querySelector("img").src = "image/icon-plus.svg";
+}
+
+
+
 firstButton.addEventListener("click", () => {
-    firstAnswer.classList.toggle("hidden");
+    const isHidden = firstAnswer.classList.contains("hidden");
 
-    const img = firstButton.querySelector("img");
+    closeAllAnswers();
 
-    if (firstAnswer.classList.contains("hidden")) {
-        img.src = "image/icon-plus.svg";
-    } else {
-        img.src = "image/icon-minus.svg";
+    if (isHidden) {
+        firstAnswer.classList.remove("hidden");
+        firstButton.querySelector("img").src = "image/icon-minus.svg";
     }
 });
+
+
 
 secondButton.addEventListener("click", () => {
-    secondAnswer.classList.toggle("hidden");
+    const isHidden = secondAnswer.classList.contains("hidden");
 
-    const img =secondButton.querySelector("img");
+    closeAllAnswers();
 
-    if (secondAnswer.classList.contains("hidden")) {
-        img.src = "image/icon-plus.svg";
-    } else {
-        img.src = "image/icon-minus.svg";
+    if (isHidden) {
+        secondAnswer.classList.remove("hidden");
+        secondButton.querySelector("img").src = "image/icon-minus.svg";
     }
 });
+
+
 
 thirdButton.addEventListener("click", () => {
-    thirdAnswer.classList.toggle("hidden");
+    const isHidden = thirdAnswer.classList.contains("hidden");
 
-    const img = thirdButton.querySelector("img");
+    closeAllAnswers();
 
-    if (thirdAnswer.classList.contains("hidden")) {
-        img.src = "image/icon-plus.svg";
-    } else {
-        img.src = "image/icon-minus.svg";
+    if (isHidden) {
+        thirdAnswer.classList.remove("hidden");
+        thirdButton.querySelector("img").src = "image/icon-minus.svg";
     }
 });
 
+
+
 fourthButton.addEventListener("click", () => {
-    fourthAnswer.classList.toggle("hidden");
+    const isHidden = fourthAnswer.classList.contains("hidden");
 
-    const img = fourthButton.querySelector("img");
+    closeAllAnswers();
 
-    if (fourthAnswer.classList.contains("hidden")) {
-        img.src = "image/icon-plus.svg";
-    } else {
-        img.src = "image/icon-minus.svg";
+    if (isHidden) {
+        fourthAnswer.classList.remove("hidden");
+        fourthButton.querySelector("img").src = "image/icon-minus.svg";
     }
 });
